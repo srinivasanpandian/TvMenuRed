9ee522cf6bf86b82d80c91e8dcc5c2daf7bd7fc1
srinivasapandian <srinivasa.pandian@maghil.com>
2025-04-18 12:53:34 +0530

    button

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.querySelector('.click-button');
+    
+    button.addEventListener('click', function() {
+        // Add your click action here
+        alert('Button clicked!');
+    });
+}); 
